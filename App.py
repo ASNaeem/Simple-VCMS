878420db5f6e8dedff49fbff3f5a0331ee921eba
Ahona <maherun.ahona.cse@ulab.edu.bd>
2023-11-23 18:20:37 +0600

    connect 3

--- App.py
+++ App.py
@@ -30,7 +30,7 @@ class MainApp(QMainWindow):
         self.button_setting.clicked.connect(self.show_setting)
         self.button_support.clicked.connect(self.show_support)
         self.button_analytics.clicked.connect(self.show_analytics_report)
-        self.button_employee.clicked.connect(self.show_employee)
+        self.button_employees.clicked.connect(self.show_employee)
         
     def show_appointment(self):
         self.stackedWidget.setCurrentWidget(self.page_appointment)
