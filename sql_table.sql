a7e88a37da6e89831e862f3f22c65248b0b1d290
Ahona <maherun.ahona.cse@ulab.edu.bd>
2023-11-25 18:06:18 +0600

    database

--- sql_table.sql
+++ sql_table.sql
@@ -27,11 +27,25 @@ create table bill()
 
 
 Create table service( id int auto_increment primary key, 
-name varchar(45),
-cost decimal(10,2),
-details varchar(50), 
-availability boolean);
+                name varchar(45),
+                cost decimal(10,2),
+                details varchar(50), 
+                availability boolean);
 
+Create table day_care( dos date,
+                start_time time,
+                end_time time,
+                s_hours int,
+                penalty_per_hour decimal(10,2),
+                status varchar(50),
+                id int);
+Create table item( id int auto_increment primary key, 
+                name varchar(45),
+                manufacture varchar(50),
+                i_type varchar(50),
+                amount int,
+                price decimal(10,2), 
+                id int);
 
 create table appointment(id int auto_increment primary key, a_time time a_date date, visit_reason varchar(100), a_status varchar(100));
 create table veterinarian(id int auto_increment primary key, spec varchar(100), totalcase int);
