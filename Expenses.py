7650c479b5baff2ff89103a98e113b3e144cc953
Orth33 <urbana.jaman.cse@ulab.edu.bd>
2023-11-30 23:54:51 +0600

    added invoice/expeses

--- Expenses.py
+++ Expenses.py
@@ -14,21 +14,53 @@ class Expenses:
          return self._invoice_id
 
     @invoice_id.setter
-    def name(self, invoice_id:str):
-         self._service_id = service_id
+    def invoice_id(self, invoice_id:int):
+         self.invoice_id = invoice_id
 
     @property
-    def service_id(self):
-         return self._service_id
+    def issuer_id(self):
+         return self._issuer_id
 
-    @name.setter
-    def name(self, service_id:str):
-         self._service_id = service_id
+    @issuer_id.setter
+    def issuer_id(self, issuer_id:int):
+         self._issuer_id = issuer_id
 
     @property
-    def service_id(self):
-         return self._service_id
+    def handler_id(self):
+         return self._handler_id
 
-    @name.setter
-    def name(self, service_id:str):
-         self._service_id = service_id
+    @handler_id.setter
+    def handler_id(self, handler_id:int):
+         self._handler_id = handler_id
+
+    @property
+    def invoice_date(self):
+         return self._invoice_date
+
+    @invoice_date.setter
+    def invoice_date(self, invoice_date:str):
+         self._invoice_date = invoice_date
+
+    @property
+    def handle_date(self):
+         return self._handle_date
+
+    @handler_id.setter
+    def handle_date(self, handle_date:str):
+         self._handle_date = handle_date
+
+    @property
+    def amount(self):
+         return self._amount
+
+    @amount.setter
+    def amount(self, amount:float):
+         self._amount = amount
+
+    @property
+    def justification(self):
+         return self._justification
+
+    @justification.setter
+    def justification(self, justification:str):
+         self._justification = justification
