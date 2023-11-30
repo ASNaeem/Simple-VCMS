512e5a60eec7c91ce8a958e4abd1432f1316234c
Orth33 <urbana.jaman.cse@ulab.edu.bd>
2023-11-30 22:20:18 +0600

    done with person, employee, veterinarian

--- Person.py
+++ Person.py
@@ -1,24 +1,28 @@
 class Person:
-    def __init__(self, name:str, phone:str, email:str, address:str):
+    def __init__(self, name:str, phone:str, email:str, password:str, address:str, employee_id:str, access_level:int, working_hours:str):
         self.name = name
         self.phone = phone
         self.email = email
+        self.password = password
         self.address = address
-          
+        self.employee_id = employee_id
+        self.access_level = access_level
+        self.working_hours = working_hours
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
-    def name(self, name:str):
+    def name(self, name: str):
         self._name = name
-        
+
     @property
     def phone(self):
         return self._phone
 
     @phone.setter
-    def phone(self, phone:str):
+    def phone(self, phone: str):
         self._phone = phone
 
     @property
@@ -26,16 +30,45 @@ class Person:
         return self._email
 
     @email.setter
-    def email(self, email:str):
+    def email(self, email: str):
         self._email = email
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password: str):
+        self._password = password
 
     @property
     def address(self):
         return self._address
 
     @address.setter
-    def address(self, address:str):
-        self._address = address    
+    def address(self, address: str):
+        self._address = address
         
-        
+    @property
+    def employee_id(self):
+        return self._employee_id
+
+    @employee_id.setter
+    def employee_id(self, employee_id: str):
+        self._employee_id = employee_id
+
+    @property
+    def access_level(self):
+        return self._access_level
     
+    @access_level.setter
+    def access_level(self, access_level:str):
+        self._access_level = access_level
+
+    @property
+    def working_hours(self):
+        return self._working_hours
+    
+    @working_hours.setter
+    def working_hours(self, working_hours:str):
+        self._working_hours = working_hours
