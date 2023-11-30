7c45048a6526cb1f46ace00c6ec9070090778f1c
ASNaeem <asaleh.naeem@gmail.com>
2023-11-30 21:28:47 +0600

    op

--- Operations.py
+++ Operations.py
@@ -1,7 +1,9 @@
+import MysqlConnectionManager as db
 import Appointment
-
 def add_appointment_db(date, time, reason:str, name:str, phone:str, address:str, animal_name:str, species:str, breed:str,
                               color:str, behaviour:str, birth:str, reg_date:str):
     apt = Appointment(date, time, re)
-    ...
     
+    cursor = db.establish_connection()
+    cursor.execute("insert into appointment()")
+
