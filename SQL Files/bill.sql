942a3219156a82cf4e736496c37e0138d3bd6e87
Tee2019 <tamanna.khatun.cse@ulab.edu.bd>
2023-12-02 01:23:53 +0600

    bill.sql

--- SQL Files/bill.sql
+++ SQL Files/bill.sql
@@ -1,29 +1,14 @@
 create table billings (
-	bid int,
+	bid int auto_increment primary key,
 	day_care_id int
 	aid int,
 	payment_date date,
 	total_amount DECIMAL(8,2),
-	status varchar(10)
+	status varchar(10) DEFAULT "Due"
 );
 
-insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (1, 1, 1, "2023-04-01" 2000.00);
-insert into billings (bid, aid, total_amount) values (2, 2, 1000.00);
-insert into billings (bid, aid, total_amount) values (3, 3, 1800.00);
-insert into billings (bid, aid, total_amount) values (4, 4, 1500.00);
-insert into billings (bid, aid, total_amount) values (5, 5, 1000.00);
-insert into billings (bid, aid, total_amount) values (6, 6, 40528.17);
-insert into billings (bid, aid, total_amount) values (7, 7, 98476.73);
-insert into billings (bid, aid, total_amount) values (8, 8, 72891.13);
-insert into billings (bid, aid, total_amount) values (9, 9, 13374.11);
-insert into billings (bid, aid, total_amount) values (10, 10, 49016.59);
-insert into billings (bid, aid, total_amount) values (11, 11, 753.72);
-insert into billings (bid, aid, total_amount) values (12, 12, 97596.8);
-insert into billings (bid, aid, total_amount) values (13, 13, 66320.01);
-insert into billings (bid, aid, total_amount) values (14, 14, 4268.68);
-insert into billings (bid, aid, total_amount) values (15, 15, 33879.89);
-insert into billings (bid, aid, total_amount) values (16, 16, 9037.03);
-insert into billings (bid, aid, total_amount) values (17, 17, 6672.00);
-insert into billings (bid, aid, total_amount) values (18, 18, 61583.00);
-insert into billings (bid, aid, total_amount) values (19, 19, 3200.00);
-insert into billings (bid, aid, total_amount) values (20, 20, 5655.00);
+insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (1,1,1,"");
+insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (2, 2, 1000.00);
+insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (3, 3, 1800.00);
+insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (4, 4, 1500.00);
+insert into billings (bid, day_care_id, aid, payment_date, total_amount, status) values (5, 5, 1000.00);
