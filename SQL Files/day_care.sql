77083201e773c65bad7562bdbf8846401500c6a4
Orth33 <urbana.jaman.cse@ulab.edu.bd>
2023-12-02 01:02:26 +0600

    hdfg

--- SQL Files/day_care.sql
+++ SQL Files/day_care.sql
@@ -1,26 +1,18 @@
-Create table day_care( animal_id int,
+Create table day_care (day_care_id int auto_increment  primary key,
+                animal_id int,
 		dos date,
                 start_time time,
                 end_time time,
-                notes TEXT);
+                notes TEXT
+);
 
-insert into day_care (dos, start_time, end_time, notes) values ("2023-05-16", "7:42 PM","9:51 PM", "allergic to nut")
-insert into day_care (dos, start_time, end_time, notes) values("2023-04-05","4:21 PM","1:15 PM","allergic to nut",2);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-02-08","1:37 PM","10:20 AM","allergic to nut",3);
-insert into day_care (dos, start_time, end_time, notes) values("2023-09-23","9:13 PM","7:01 PM",5,24.12,"allergic to nut",4);
-insert into day_care (dos, start_time, end_time, notes) values("2023-01-11","11:34 AM","6:16 PM",7,76.52,"allergic to nut",5);
-insert into day_care (dos, start_time, end_time, notes) values("2023-03-02","9:31 PM","9:31 AM",4,92.53,"allergic to nut",6);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-01-14","4:12 PM","6:59 PM",10,20.05,"allergic to choco",7);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-01-18","10:30 AM","11:41 AM",10,26.77,"allergic to choco",8);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-10-13","1:37 PM","9:25 AM",3,32.37,"allergic to nut",9);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-04-30","8:13 PM","4:30 PM",10,1.77,"allergic to choco",10);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-06-01","9:49 AM","6:42 PM",5,32.64,"allergic to nut",11);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-06-23","8:50 PM","7:05 PM",1,62.2,"allergic to choco",12);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-01-16","10:41 AM","12:41 AM",2,76.99,"allergic to choco",13);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-10-03","7:01 PM","1:44 PM",1,49.33,"allergic to choco",14);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-07-04","8:57 PM","9:57 PM",1,46.5,"allergic to choco",15);
-insert into day_care (dos, start_time, end_time, notes) values ("2022-11-27","3:00 PM","2:09 PM",5,18.72,"allergic to choco",16);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-03-04","8:33 PM","10:05 AM",2,7.8,"allergic to nut",17);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-06-21","6:00 PM","2:19 PM", 6, 54.38,"allergic to choco",18);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-11-22","5:54 PM","7:55 PM",6,98.66,"allergic to nut",19);
-insert into day_care (dos, start_time, end_time, notes) values ("2023-06-16","3:23 PM","10:12 AM",6,26.19,"allergic to nut",20);
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (1, '2020-01-25', 0.51, 14.68, 'allergic to dairy');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (8, '2022-12-28', 6.09, 5.63, 'allergic to egg');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (9, '2021-08-12', 5.33, 14.86, 'allergic to chicken');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (4, '2021-01-05', 17.7, 17.49, 'allergic to chicken');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (9, '2020-10-31', 12.59, 18.29, 'allergic to egg');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (6, '2022-05-28', 22.07, 6.11, 'allergic to nut');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (6, '2020-06-10', 15.96, 13.34, 'allergic to chicken');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (3, '2020-03-08', 0.24, 16.87, 'allergic to nut');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (1, '2022-03-02', 8.62, 16.64, 'allergic to beef');
+insert into day_care (animal_id, dos, start_time, end_time, notes) values (3, '2020-12-26', 21.93, 20.84, 'allergic to egg');
