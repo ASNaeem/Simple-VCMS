e8296f9d5926e6dbb85607bb875c1b4392f7b9fc
Orth33 <urbana.jaman.cse@ulab.edu.bd>
2023-12-06 16:39:22 +0600

    animal

--- App.py
+++ App.py
@@ -478,6 +478,8 @@ class MainApp(QMainWindow):
         table.setItem(row, 1, QTableWidgetItem(str(record[0])))
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
 
+    ################# Animal End #################
+
     ################# Employee ###################
 
     def clear_employee_fields():
@@ -623,7 +625,6 @@ class MainApp(QMainWindow):
         header.setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
         table.setItem(row, 5, QTableWidgetItem(str(day_care.notes)))
         header.setSectionResizeMode(5, QtWidgets.QHeaderView.ResizeToContents)"""
-
         table.setItem(row, 0, QTableWidgetItem(str(day_care.day_Care_id)))
         table.setItem(row, 1, QTableWidgetItem(str(day_care.animal_id)))
         table.setItem(row, 2, QTableWidgetItem(str(day_care.day_care_date)))
@@ -657,7 +658,8 @@ class MainApp(QMainWindow):
         table.setItem(row, 4, QTableWidgetItem(str(expense.justification)))
         header.setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
 
-        # self.resize_columns_to_contents_alternate(table)
+        #self.resize_columns_to_contents_alternate(table)
+        #No need to resize the expense table
 
     ################### Billing ##################
 
@@ -692,6 +694,12 @@ class MainApp(QMainWindow):
         header.setSectionResizeMode(5, QtWidgets.QHeaderView.ResizeToContents)
         table.setItem(row, 6, QTableWidgetItem(str(billing.status)))
         header.setSectionResizeMode(6, QtWidgets.QHeaderView.ResizeToContents)
+        '''self.resize_columns_to_contents_alternate(table)
+        table.resizeColumnToContents(0)
+        table.resizeColumnToContents(1)
+        table.resizeColumnToContents(2) '''
+
+                   
 
     def add_billing_service_to_service_table(self, rowService, service):
         header = self.page_billing.table_show_service.horizontalHeader()
@@ -707,6 +715,7 @@ class MainApp(QMainWindow):
 
         table.setItem(rowService, 2, QTableWidgetItem(service.cost))
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+
 
     ################### End Billing ##################
 
